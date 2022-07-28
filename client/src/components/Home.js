317b133a4ed9281f490ea2805246b8cfd466f4f6
Merle Tyson Mills <tysonmills5@gmail.com>
2022-07-28 12:44:02 -0500

    react-scripts to v5

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -3,14 +3,23 @@ import { Link } from "react-router-dom";
 import Login from './Login';
 
 
-function Home() {
-
+function Home( {parks} ) {
+  console.log(parks.data)
+  const parksData = parks.data
 
   return(
+
     <>
       <h1>HOMEPAGE</h1>
       <Login />
       <h1><Link to='/signup'>Sign Up</Link></h1>
+      <div key="index">
+        {parksData.map(park => (
+          <>
+          <h3>{park.fullName}</h3>
+          </>
+        ))}
+      </div>
     </>
     
   )
